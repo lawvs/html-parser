2c7bc3d9a6649d2136afd0b5480269b03f6966d3
lawvs <lawvs@users.noreply.github.com>
2020-09-29 04:03:43 +0900

    chore: add tests

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,3 +1,42 @@
-describe('base', () => {
-  test('should parser works', () => {})
-})
+import { parseHTML } from "..";
+
+describe.skip("base", () => {
+  test("should parser works", () => {
+    const nodes = parseHTML(
+      '<div id="main" data-x="hello">Hello<span id="sub" /></div>'
+    );
+    expect(nodes).toStrictEqual({
+      tag: "div",
+      selfClose: false,
+      attributes: {
+        id: "main",
+        "data-x": "hello",
+      },
+      text: "Hello",
+      children: [
+        {
+          tag: "span",
+          selfClose: true,
+          attributes: {
+            id: "sub",
+          },
+        },
+      ],
+    });
+  });
+});
+
+describe("Text", () => {
+  test("simple text", () => {
+    const ast = parseHTML("some text");
+
+    expect(ast).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "content": "some text",
+          "type": 1,
+        },
+      ]
+    `);
+  });
+});
